c9c93362eebf3862b2956b658ed00279c58341b9
Sam Baynham <sam.baynham@gmail.com>
2021-01-23 13:28:25 +0000

    Adding stray interface

---
+++ src/Domain/Interfaces/SluggableDomainEntityInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Fifthgate\Objectivity\Core\Domain\Interfaces;
+
+use Fifthgate\Objectivity\Core\Domain\Interfaces\DomainEntityInterface;
+use \DateTimeInterface;
+
+interface SluggableDomainEntityInterface extends DomainEntityInterface
+{
+    public function getSlug() : string;
+
+    public function setSlug() : string;
+}
