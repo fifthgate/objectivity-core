6b075451e9d25d5c1da5a5421614a70f8705b479
Sam Baynham <sam@fifthgate.net>
2021-09-30 19:53:30 +0100

    Adding reversion test for shadowvalue function

--- tests/mocks/MockSerializableDomainEntity.php
+++ tests/mocks/MockSerializableDomainEntity.php
@@ -5,27 +5,40 @@ namespace Fifthgate\Objectivity\Core\Tests\Mocks;
 use Fifthgate\Objectivity\Core\Domain\AbstractSerializableDomainEntity;
 use Fifthgate\Objectivity\Core\Domain\Interfaces\JsonSerializableDomainEntityInterface;
 use Fifthgate\Objectivity\Core\Domain\Traits\JsonSerializes;
+use Fifthgate\Objectivity\Core\Domain\Traits\ShadowsValues;
 
-class MockSerializableDomainEntity extends AbstractSerializableDomainEntity implements JsonSerializableDomainEntityInterface {
+class MockSerializableDomainEntity extends AbstractSerializableDomainEntity implements JsonSerializableDomainEntityInterface
+{
 
     use JsonSerializes;
+
+    use ShadowsValues;
     
     protected string $dummyStringValue;
 
     protected string $dummySlugValue;
 
-    public function setDummySlugValue(string $dummySlugValue) {
+    public function setDummySlugValue(string $dummySlugValue)
+    {
         $this->dummySlugValue = $dummySlugValue;
     }
 
-    public function getDummySlugValue() : string {
+    public function getDummySlugValue() : string
+    {
         return $this->dummySlugValue;
     }
-    public function setDummyStringValue(string $dummyStringValue) {
+    public function setDummyStringValue(string $dummyStringValue)
+    {
         $this->dummyStringValue = $dummyStringValue;
     }
 
-    public function getDummyStringValue() : string {
+    public function getDummyStringValue() : string
+    {
         return $this->dummyStringValue;
+    }
+
+    public function isShadowableValue(string $shadowName) : bool
+    {
+        return true;
     }
 }
